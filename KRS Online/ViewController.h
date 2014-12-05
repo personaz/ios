d25f599962263d81a85bef23094bf3763019d224
Naufal Dzaki Lubis <Naufal@Naufal-Dzaki-Lubiss-MacBook-Pro.local>
2014-12-05 22:14:43 +0700

    Initial Commit

---
+++ KRS Online/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KRS Online
+//
+//  Created by Naufal Dzaki Lubis on 12/5/14.
+//  Copyright (c) 2014 Zulham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
