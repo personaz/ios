f4f60d72e11e47da8892f9d603bea544a127b922
Surya Winata <suryawinata@nusa.net.id>
2015-03-24 22:37:17 +0700

    tambah model mapper

---
+++ KRS Online/ModelMapper.h
@@ -0,0 +1,13 @@
+//
+//  Helper.h
+//  KRS Online
+//
+//  Created by Muhammad Zulham on 3/17/15.
+//  Copyright (c) 2015 Zulham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ModelMapper : NSObject
+
+@end
---
+++ KRS Online/ModelMapper.m
@@ -0,0 +1,13 @@
+//
+//  Helper.m
+//  KRS Online
+//
+//  Created by Muhammad Zulham on 3/17/15.
+//  Copyright (c) 2015 Zulham. All rights reserved.
+//
+
+#import "ModelMapper.h"
+
+@implementation ModelMapper
+
+@end
